6e6a6f743edc5dc8b667a804b62f6105b393b3e0
Milan <milanpavlovic02@icloud.com>
2025-06-09 19:04:01 +0200

    chore: update mt login page on md

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "./_components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted mt-20 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted mt-20 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:mt-0 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
