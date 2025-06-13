7d9b28151724c1c3d4dc64d1bfb59e087e94302a
Milan <milanpavlovic02@icloud.com>
2025-06-13 17:43:32 +0200

    Refactor layout and styles; remove unused page and update editor styles
    - Changed global CSS import to SCSS in layout.tsx.
    - Deleted unused simple page component.
    - Updated simple editor styles to improve layout and responsiveness.
    - Adjusted toolbar styles for better visibility and interaction.
    - Added keyframe animations and variables for enhanced styling options.

---
+++ app/add-blog/page.tsx
@@ -0,0 +1,9 @@
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+
+export default function Page() {
+  return (
+    <div className="mt-20 px-4">
+      <SimpleEditor />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Header } from "@/components/shared/header/header";
