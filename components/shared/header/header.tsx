cbf0a855e25a028ae6587b2a66b82c84767c5690
Milan <107366981+Lanmi2304@users.noreply.github.com>
2025-07-05 12:42:02 +0200

    Update header.tsx

--- components/shared/header/header.tsx
+++ components/shared/header/header.tsx
@@ -7,7 +7,7 @@ export async function Header() {
   const data = await auth.api.getSession({ headers: await headers() });
   const user = data?.user;
   return (
-    <header className="bg-background/50 fixed top-0 left-[50%] z-50 flex h-20 w-full max-w-4xl shrink-0 translate-x-[-50%] transform items-center border-b backdrop-blur-md md:px-6">
+    <header className="bg-background/50 fixed top-0 left-[50%] z-50 flex h-20 w-full max-w-4xl px-4 shrink-0 translate-x-[-50%] transform items-center border-b backdrop-blur-md">
       <NavigationMenu className="flex w-full max-w-full justify-start">
         <MenuItems user={user} />
       </NavigationMenu>
